9ece1a44032a43995677a8e7220c67fa31b7e344
Syohei YOSHIDA <syohex@gmail.com>
2014-08-20 00:03:45 +0900

    Use octal character code instead of ^M
    Because someone replace ^M with \n at before-save-hook.
    ^M is not good when such people pull-request.

--- codic.el
+++ codic.el
@@ -67,7 +67,7 @@
              for start = (1+ (point))
              do
              (progn
-               (while (re-search-forward "$" (line-end-position) t)
+               (while (re-search-forward "\015$" (line-end-position) t)
                  (forward-line 1))
                (let* ((line (buffer-substring-no-properties
                              start (1- (line-end-position))))
